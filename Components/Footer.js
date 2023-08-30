c0e30faf8d67fd022b2d7dd66bc883b49e5be84c
Alireza <alirezanoun14@gmail.com>
2023-08-29 23:52:19 -0400

    first commit

---
+++ Components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = `
+<div class="footer__container">
+	<p class="website__rights" id="sign-up">© G-ENGINE 2023</p>
+</div>
+`;
+
+export default Footer;
---
+++ Components/Navbar.js
@@ -0,0 +1,41 @@
+const Navbar = `
+
+<nav class="navbar">
+	<header class="navbar__toggle__wrapper">
+		<div class="navbar__toggle" id="mobile-menu">
+			<span class="bar"></span>
+			<span class="bar"></span>
+			<span class="bar"></span>
+		</div>
+	</header>
+
+	<footer class="navbar__container">
+		<ul class="navigation-items">
+			<li class="navbar__item">
+				<a href="./index.html" class="navbar__links" id="about-page">about</a>
+			</li>
+			<li class="navbar__item">
+				<a href="./work.html" class="navbar__links" id="work-page">services</a>
+			</li>
+		</ul>
+
+		<a class="logo_area" href="./index.html">
+			<img src="./img/Untitled design.svg" />
+		</a>
+
+		<ul class="navigation-items">
+			<li class="navbar__item">
+				<a href="https://www.instagram.com/zgraphicals/" target="_blank" class="navbar__links" id="insta">hours</a>
+			</li>
+			<li class="navbar__item">
+				<a href="./contact.html" class="navbar__links" id="work-page">book</a>
+			</li>
+		</ul>
+	</footer>
+</nav>
+
+
+
+`;
+
+export default Navbar;
